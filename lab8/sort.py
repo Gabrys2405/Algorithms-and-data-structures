df5f34302b607ab7c61e2b2c218d758ac5367a55
Gabrys2405 <majgabriel@student.agh.edu.pl>
2022-04-29 22:59:24 +0200

    sort TODO

--- lab8/sort.py
+++ lab8/sort.py
@@ -14,13 +14,16 @@ class Element:
 
 
 class PrioQueue:
-    def __init__(self):
-        self.tab = []
+    def __init__(self,tab = None):
+        self.tab = tab
+        self.tab_size = len(self.tab)
         self.size = 0
     def is_empty(self):
         if self.size: return False
         else: return True
-
+    def heapify(self):
+        for i in range(self.tab_size):
+            self.repair(i)
     def enqueue(self,priority,data):
         element = Element(priority,data)
         if self.is_empty():
@@ -51,45 +54,47 @@ class PrioQueue:
             return buf.data
         else:
             result = self.tab[0]
-           
             self.tab[0] = self.tab[self.size - 1]
             self.tab[self.size - 1] = 0
-            self.tab.remove(0)
             self.size -= 1
-            buffer = self.tab[0]
             r_in = self.right(0)
             l_in = self.left(0)
             if r_in != self.size and l_in != self.size:
-                
-                while (self.tab[r_in] > buffer or self.tab[l_in] > buffer):
-                    if self.tab[r_in] > self.tab[l_in]:
-                        self.tab[self.parent(r_in)] = self.tab[r_in]
-                        self.tab[r_in] = buffer
-                        buffer = self.tab[r_in]
-                        buf2 = r_in
-                        l_in = self.left(r_in)
-                        r_in = self.right(r_in)
-
-                        
-                    else:
-                        self.tab[self.parent(l_in)] = self.tab[l_in]
-                        self.tab[l_in] = buffer
-                        buffer = self.tab[l_in]
-                        buf2 = r_in
-                        r_in = self.right(l_in)
-                        l_in = self.left(l_in)
-                    if l_in > self.size or r_in >= self.size:
-                        break
+                self.repair(0)
             return result.data
 
-
+    def repair(self,index):
+        buffer = self.tab[index]
+        r_in = self.right(index)
+        l_in = self.left(index)
+        while (self.tab[r_in] > buffer or self.tab[l_in] > buffer):
+            if self.tab[r_in] > self.tab[l_in]:
+                self.tab[self.parent(r_in)] = self.tab[r_in]
+                self.tab[r_in] = buffer
+                buffer = self.tab[r_in]
+                l_in = self.left(r_in)
+                r_in = self.right(r_in)
+            else:
+                self.tab[self.parent(l_in)] = self.tab[l_in]
+                self.tab[l_in] = buffer
+                buffer = self.tab[l_in]
+                r_in = self.right(l_in)
+                l_in = self.left(l_in)
+            if l_in > self.size or r_in >= self.size:
+                break
 
 
     def right(self, indeks):
-        return(indeks * 2) + 2 
+        result = (indeks * 2) + 2
+        if result < self.tab_size:
+        
+            return result  
 
     def left(self, indeks):
-        return(indeks * 2) + 1
+        result = (indeks * 2) + 1
+        if result < self.tab_size:
+        
+            return result 
     def parent(self, indeks):
         return ( indeks - 1) // 2
     def peek(self):
@@ -117,17 +122,15 @@ class PrioQueue:
 
 def main():
 
-    prio = PrioQueue()
-   
-    tab = [4,7,6,7,5,2,2,1]
-    string = 'ALGORYTM'
-    for i in range(len(tab)):
-        prio.enqueue(tab[i], string[i])
 
+    prio = PrioQueue([Element(pr,dat)for pr, dat in [(5,'A'), (5,'B'), (7,'C'), (2,'D'), (5,'E'), (1,'F'), (7,'G'), (5,'H'), (1,'I'), (2,'J')]])
+    
+    prio.heapify()
+    
     prio.print_tree(0,0)
     prio.print_tab()
     
-    print(prio.dequeue())
+    
   
     print(prio.peek())
     prio.print_tab()
@@ -137,10 +140,39 @@ def main():
         if element:
             print(element)
     prio.print_tab()
+    print('TEST')
 
+
+
+    # prio = PrioQueue()
+   
+    # tab = [4,7,6,7,5,2,2,1]
+    # string = 'ALGORYTM'
+    # for i in range(len(tab)):
+    #     prio.enqueue(tab[i], string[i])
+
+    # prio.print_tree(0,0)
+    # prio.print_tab()
+    
+    # print(prio.dequeue())
+  
+    # print(prio.peek())
+    # prio.print_tab()
+    # element = 1
+    # while element:
+    #     element = prio.dequeue()
+    #     if element:
+    #         print(element)
+    # prio.print_tab()
 
 if __name__ == "__main__":
     main()
+
+    
+
+
+
+
 
     
 
